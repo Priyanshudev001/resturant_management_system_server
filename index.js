fc0314a1d4742fcad7ffa8c9fabecd6487ab3634
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-04-03 12:04:37 +0545

    express json added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const prisma = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 const app = express();
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 const { PrismaClient } = prisma;
 const prismaClient = new PrismaClient();
 
